4f57b157ce2f6a871af8d72d45b9cbeaef8f5961
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-10-05 16:46:30 -0300

    Including method to consult fact in database

--- src/database.js
+++ src/database.js
@@ -14,6 +14,42 @@ var Database = function () {
         var fact = new Fact(parsedFact.predicate, parsedFact.parameters);
         facts.push(fact);
     }
+    
+    var samePredicate = function(clause, query) {
+        return clause.obtainPredicate() === query.predicate;
+    }
+    
+    var sameParameters = function(clause, query) {
+        var clauseParameters = clause.obtainParameters();
+        var queryParameters = query.parameters;
+        
+        if (clauseParameters.length != queryParameters.length) {
+            return false;
+        }
+        
+        var parametersLength = clauseParameters.length;
+        for (var i = 0; i < parametersLength; i++) {
+            if (clauseParameters[i] != queryParameters[i])
+                return false;
+        }
+        
+        return true;
+    }
+    
+    this.hasFact = function(fact) {
+        var factsListLength = facts.length;
+
+        for (var i = 0; i < factsListLength; i++) {
+            var dbFact = facts[i];
+            if (samePredicate(dbFact, fact) && sameParameters(dbFact, fact)) {
+                return true;
+            }
+        }
+	
+        return false;
+    }
+    
+    
 }
 
 module.exports = Database;
