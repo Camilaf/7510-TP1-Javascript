699c46ce5f40dcebcb4ec191595f385c6f87a2b3
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-10-07 02:09:47 -0300

    Adding parseDB and checkQuery implementations

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,11 +1,34 @@
+var Database = require('./database');
+var Parser = require('./parser');
+
 var Interpreter = function () {
-
-    this.parseDB = function (params, paramss, paramsss) {
-
+    var database = new Database();
+    var parser = new Parser();
+    
+    this.parseDB = function (databaseList) {
+        var databaseListLength = databaseList.length;
+        
+        for (var i = 0; i < databaseListLength; i++) {
+            var clause = databaseList[i];
+            var cleanClause = parser.removeAllSpaces(clause);
+            
+            if (parser.isFact(cleanClause)) {
+                var parsedFact = parser.parseFact(cleanClause);
+                database.addFact(parsedFact);
+                
+            } else if (parser.isRule(cleanClause)) {
+                var parsedRule = parser.parseRule(cleanClause);
+                database.addRule(parsedRule);
+                
+            } else {
+                throw new Error('Invalid entry in database: ' + clause);
+            }
+        }
     }
 
-    this.checkQuery = function (params) {
-        return true;
+    this.checkQuery = function (query) {
+        var parsedQuery = parser.parseQuery(query);      
+        return (database.hasFact(parsedQuery) || database.hasRule(parsedQuery));
     }
 
 }
